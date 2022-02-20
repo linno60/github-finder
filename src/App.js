770134eb79615340b6690fb4e047e62c07c65c59
Lin Liu <liulinn60@gmail.com>
2022-02-20 12:43:45 -0800

    adding Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <button>Button</button>
-    </div>
-  );
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+
+        <main>Content</main>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
