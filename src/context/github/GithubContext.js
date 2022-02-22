e1344824ae4a2a0d0efdda02b0716d99ac2ea65f
Lin Liu <liulinn60@gmail.com>
2022-02-21 18:27:49 -0800

    updating user profile part1

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,5 +1,4 @@
 import { createContext, useReducer } from 'react';
-import { resolvePath } from 'react-router-dom';
 import githubReducer from './GithubReducer';
 
 const GithubContext = createContext()
