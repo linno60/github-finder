8702499890ec4cca589044a61ad118263d098fad
Lin Liu <liulinn60@gmail.com>
2022-02-22 16:56:50 -0800

    clean up action file

--- src/context/github/GithubActions.js
+++ src/context/github/GithubActions.js
@@ -1,45 +1,29 @@
+import axios from 'axios'
+
+const github = axios.create({
+    baseURL: 'https://api.github.com'
+})
+
 // Get search results
 export const searchUsers = async (text) => {
-       const params = new URLSearchParams({
+    const params = new URLSearchParams({
         q: text
     })
 
-    const response = await fetch(`https://api.github.com/search/users?${params}`
-    // , {
-    //     headers: {
-    //         Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-    //     }
-    // }
-    )
-
-    const {items} = await response.json()
-
-    return items
+    const response = await github.get(`/search/users?${params}`)
+    return response.data.items
 }
 
-// Get single user
-export const getUser = async (login) => {
-    const response = await fetch(`https://api.github.com/users/${login}`)
-
-    if(response.status === 404) {
-        window.loacation = '/notfound'
-    } else {
-        const data = await response.json()
-
-       return data
-    }        
-}
-
-// Get user repos
-export const getUserRepos = async (login) => {
+// Get user and repos
+export const getUserAndRepos = async(login) => {
     const params = new URLSearchParams({
         sort: 'created',
         per_page: 10
     })
+    const [user, repos] = await Promise.all([
+        github.get(`/users/${login}`),
+        github.get(`users/${login}/repos?${params}`)
+    ])
 
-    const response = await fetch(`https://api.github.com/users/${login}/repos?${params}`)
-
-    const data = await response.json()
-
-    return data
+    return { user: user.data, repos: repos.data}
 }
